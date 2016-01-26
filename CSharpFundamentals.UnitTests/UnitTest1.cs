6eaff875af2213090080348e333d8dfbd2aa5ec1
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2016-01-26 19:06:50 +0500

    Initial commit

---
+++ CSharpFundamentals.UnitTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpFundamentals.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ArrayInitialize()
+        {
+            int num = 4;
+            int[] var = new int[num];
+        }
+
+        [TestMethod]
+        public void StringParameter()
+        {
+            string str = "A string";
+            changeString(str);
+            Assert.AreNotEqual("Changes string", str);
+        }
+
+        private void changeString(string str)
+        {
+            str = "Changes string";
+        }
+
+        [TestMethod]
+        public void StringObjectParameter()
+        {
+            String str = "A string";
+            changeString(str);
+            Assert.AreNotEqual("Changes string", str);            
+        }
+
+        private void changeStringObject(String str)
+        {
+            str = "Changes string";
+        }
+    }
+}
